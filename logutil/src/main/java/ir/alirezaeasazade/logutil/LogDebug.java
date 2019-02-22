650fc7dc510231dfc50930d581a639b7e131ea41
alireza <easazade@gmail.com>
2019-02-22 14:06:26 +0330

    init

---
+++ logutil/src/main/java/ir/alirezaeasazade/logutil/LogDebug.java
@@ -0,0 +1,13 @@
+package ir.alirezaeasazade.logutil;
+
+import android.util.Log;
+
+public class LogDebug {
+
+    private static final String TAG = "SUPPER_AWESOME_APP";
+
+    public static void d(String message) {
+        Log.d("TAG", message);
+    }
+
+}
